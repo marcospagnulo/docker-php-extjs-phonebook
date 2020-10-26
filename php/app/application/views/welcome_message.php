1cff9d15de42675b9a975d3559f42b93d01459e3
Marco Spagnulo <marco.spagnulo@gmail.com>
2020-10-26 18:03:59 +0100

    Folder refactoring

---
+++ php/app/application/views/welcome_message.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<?php
+	phpinfo();
+	?>
+
+</div>
+
+</body>
+</html>
